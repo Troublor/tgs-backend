515333b7c563160a07e7fc652dc83f58521e0d52
Wuqi Aaron Zhang <troublor@live.com>
2022-01-13 18:52:43 +0800

    :sparkles: add version api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import fs from 'fs';
+import * as path from 'path';
+import { appRoot } from './config/loader';
 
 @Controller('/')
 export class AppController {
   @Get('/ping')
   getPing(): string {
     return 'Yes, you got it right!';
+  }
+
+  @Get('/version')
+  getVersion(): string {
+    const packageJson = JSON.parse(
+      fs.readFileSync(path.join(appRoot, 'package.json'), 'utf8'),
+    );
+    return 'v' + packageJson.version;
   }
 }
