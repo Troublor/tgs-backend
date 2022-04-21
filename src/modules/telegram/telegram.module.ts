8e4c8fcbef8b8fb48e988fa09bb5db8db2d10f3a
William Aaron Cheung <troublor@live.com>
2022-04-21 23:55:34 +0800

    :bug: fix regression issues

--- src/modules/telegram/telegram.module.ts
+++ src/modules/telegram/telegram.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TelegramController } from './telegram.controller.js';
 import NotifierBotService from './notifier-bot.service.js';
 import DatabaseModule from '../database/database.module.js';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [TelegramController],
   providers: [NotifierBotService],
   exports: [NotifierBotService],
 })
