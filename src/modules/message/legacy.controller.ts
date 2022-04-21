98b8287fde43aedec88b78040c5f9a38a8780689
William Aaron Cheung <troublor@live.com>
2022-04-21 23:54:33 +0800

    :bug: fix regression issues

---
+++ src/modules/message/legacy.controller.ts
@@ -0,0 +1,76 @@
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiConsumes,
+  ApiNotFoundResponse,
+} from '@nestjs/swagger';
+import UserService from '../database/user.service.js';
+import MessageService from './message.service.js';
+import { PlainBody } from '../plain-body.decorator';
+
+@Controller()
+export default class LegacyController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly messageService: MessageService,
+  ) {}
+
+  @Get('/telegram/notify/:userId/:message')
+  @ApiBadRequestResponse({ description: 'Empty message to send' })
+  @ApiNotFoundResponse({ description: 'No chat associated with userId' })
+  async legacyNotify(
+    @Param('message') msg: string,
+    @Param('userId') username: string,
+  ) {
+    const user = await this.userService.getUser(username);
+    if (!user) throw new NotFoundException('username not found');
+    let count: number;
+    try {
+      count = await this.messageService.broadcastMessageToTelegram(user, msg);
+    } catch (e) {
+      throw new NotFoundException(e);
+    }
+    if (count > 0) {
+      return `Message sent to user '${username}': \n${msg}`;
+    } else {
+      throw new NotFoundException('No chat associated with username');
+    }
+  }
+
+  @Post('/telegram/notify/:userId')
+  @ApiConsumes('text/plain')
+  @ApiBadRequestResponse({ description: 'Empty message to send' })
+  @ApiNotFoundResponse({ description: 'No chat associated with userId' })
+  async legacyPostNotify(
+    @PlainBody() raw: string,
+    @Param('userId') username: string,
+  ) {
+    const text = raw.trim();
+    if (text.length == 0) {
+      throw new BadRequestException(new Error('empty message'));
+    }
+    const user = await this.userService.getUser(username);
+    if (!user) throw new NotFoundException('username not found');
+    let count: number;
+    try {
+      count = await this.messageService.broadcastMessageToTelegram(user, text);
+    } catch (e) {
+      throw new NotFoundException(e);
+    }
+    if (count > 0) {
+      return `**NOTICE: this API is deprecated.
+      Use "curl -X POST https://troublor.xyz/message/telegram/<username>[/<message>] -H ['Content-Type: text/plain' -d <message>]" instead.
+
+      Message sent to user '${username}': \n${text}`;
+    } else {
+      throw new NotFoundException('No chat associated with username');
+    }
+  }
+}
--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -3,10 +3,11 @@ import DatabaseModule from '../database/database.module.js';
 import MessageService from './message.service.js';
 import MessageController from './message.controller.js';
 import { TelegramModule } from '../telegram/telegram.module.js';
+import LegacyController from './legacy.controller.js';
 
 @Module({
   imports: [DatabaseModule, TelegramModule],
   providers: [MessageService],
-  controllers: [MessageController],
+  controllers: [MessageController, LegacyController],
 })
 export default class MessageModule {}
